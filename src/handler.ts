9b607472aa4d5a932b45a450910bcc31bbe801ac
Qizhongdd <1090018789@qq.com>
2021-11-14 17:54:07 +0800

    s

--- src/handler.ts
+++ src/handler.ts
@@ -2,7 +2,6 @@ export async function handleRequest(): Promise<Response> {
 
  const ethPrice= await TODO.get('ETH',{cacheTtl: 3600})
  const btbPrice=await TODO.get('BNB',{cacheTtl: 3600})
- console.log('aa')
   return new Response(JSON.stringify({
     eth:ethPrice,
     btb:btbPrice
